b0d6419059819bab62deab47d2f6761e134220cd
aa991102 <63843905+aa991102@users.noreply.github.com>
2021-02-14 23:53:34 +0900

    21.02.14 - 스프링 버전 변경 3 -> 4 에 따른 오류 발생.
    MySQL연결 확인하려고 TestMySQL.class 생성해서 하는데 "unsupported major.minor version 52.0" 오류 발생. 이는 Java Version 52.0 (JDK 8.0) 버전에서 컴파일하고 이하 버전에서 실행할 때 발생한다고 함.
    
    -> 자바 버전을 1.8로 바꾸고, 이에 따라 스프링 버전도 3.2.0 -> 4.3.4 로 변경.
    
    -> 근데 오류 엄청나게 발생함. 이건 내일의 나에게 맡긴다...

---
+++ src/test/java/com/project/myver/TestMySQL.java
@@ -0,0 +1,26 @@
+package com.project.myver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestMySQL {
+	public static void main(String[] args) {
+		try {
+		    Class.forName("com.mysql.jdbc.Driver");
+		} 
+		catch (ClassNotFoundException e) {
+		    // TODO Auto-generated catch block
+		    e.printStackTrace();
+		}
+		
+		Connection conn;
+		
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/myver","jiyoung","1234");
+			System.out.println("데이터베이스 연결 성공");
+			conn.close();
+		}catch(Exception e) {
+			System.out.println("오류:"+e);
+		}
+	}
+}
